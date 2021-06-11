8385d1f0ccd16bdaf872afece86d93556edd7145
“Genaro <ramoon1996@hotmail.com>
2021-06-11 18:38:06 +0200

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import contacts from "./contacts.json";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: contacts.slice(0,5),
+    }
+  }
+
+  render() {
+
+    const allContacts = [...this.state.contacts]
+    return(
+        <div className="container-table">
+          <h2>IronContacts</h2>
+          <table>
+              <thead>
+                  <tr>
+                    <th>Picture</th>
+                    <th>Name</th>
+                    <th>Popularity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                {allContacts.map(contact => {
+                const {name, popularity, pictureUrl} = contact;
+
+                  return(
+                    <tr>
+                      <td><img height='100px'src={pictureUrl}/></td>
+                      <td>{name}</td>
+                      <td>{popularity.toFixed(2)}</td>
+                    </tr>
+                  ) 
+            }
+          )
+        }
+                  </tbody>
+            </table>
+        </div>
+    )
+  }
+} 
 
 export default App;
